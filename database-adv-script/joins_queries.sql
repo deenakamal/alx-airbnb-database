74e130033486c5192dcae59e79d6e76db114c26a
Deena <133583413+deenakamal@users.noreply.github.com>
2025-07-07 00:23:48 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,38 @@
+-- Query 1: INNER JOIN bookings with users
+SELECT
+    Booking.booking_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    Booking.status,
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    User.email
+FROM Booking
+INNER JOIN User ON Booking.user_id = User.user_id;
 
+-- Query 2: LEFT JOIN properties with reviews
+SELECT
+    Property.property_id,
+    Property.name,
+    Property.location,
+    Review.review_id,
+    Review.rating,
+    Review.comment
+FROM Property
+LEFT JOIN Review ON Property.property_id = Review.property_id;
+
+-- Query 3: FULL OUTER JOIN users with bookings
+SELECT
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    User.email,
+    Booking.booking_id,
+    Booking.start_date,
+    Booking.end_date,
+    Booking.total_price,
+    Booking.status
+FROM User
+FULL OUTER JOIN Booking ON User.user_id = Booking.user_id;
